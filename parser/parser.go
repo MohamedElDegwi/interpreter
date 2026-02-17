43d6d68f58fc8cc02efd0731e5c0e00a64e1abcf
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-02-17 21:06:39 +0000

    feat: implement return statement parsing
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- parser/parser.go
+++ parser/parser.go
@@ -59,6 +59,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -79,6 +81,17 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
 	// TODO: We're skipping the expression untill we
 	// encounter a semicolon or EOF
+	for !p.curTokenIs(token.SEMICOLON) && !p.curTokenIs(token.EOF) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
 	for !p.curTokenIs(token.SEMICOLON) && !p.curTokenIs(token.EOF) {
 		p.nextToken()
 	}
