b301eccf144b3cb796b6bb37a40b0ec9a30f66e9
Mohammed ElDegwi <mohammedeldegwi@gmail.com>
2026-02-20 08:32:44 +0000

    fix: address repl malfunction
    Signed-off-by: Mohammed ElDegwi <mohammedeldegwi@gmail.com>

--- repl/repl.go
+++ repl/repl.go
@@ -23,20 +23,19 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-
 		l := lexer.New(line)
 		p := parser.New(l)
 
 		program := p.ParseProgram()
-
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
+			continue
 		}
 
 		evaluated := evaluator.Eval(program)
 
 		if evaluated != nil {
-			io.WriteString(out, program.String())
+			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
 		}
 	}
